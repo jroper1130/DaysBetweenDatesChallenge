9c7e63d93d43c61d6ec4f21376c825f7499a1594
jroper1130 <warbeast78@hotmail.com>
2017-10-31 02:49:09 -0700

    Add files via upload

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeDaysBetweenDates.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How many days have elapsed?<br />
+            <br />
+            Choose one date:<asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+            <br />
+            Choose a second date:<asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ok" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Calendar1.SelectedDate > Calendar2.SelectedDate)
+            {
+                Label1.Text = Calendar1.SelectedDate
+                .Subtract(Calendar2.SelectedDate)
+                .TotalDays.ToString();
+
+            }
+            else
+            {
+                Label1.Text = Calendar2.SelectedDate
+                .Subtract(Calendar1.SelectedDate)
+                .TotalDays.ToString();
+            }
+
+        }
+    }
+}
